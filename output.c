bd30eb54e09dce26dd61f9607ecc9d6cdcb0624b
mckset <99543950+mckset@users.noreply.github.com>
2023-01-25 20:20:49 -0500

    Added mouse button support

--- output.c
+++ output.c
@@ -1,14 +1,22 @@
 #include <xdo.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int args, char *in[]){
 	xdo_t *x = xdo_new(NULL);
-	if (strcmp(in[2], "0") == 0){		
-		xdo_send_keysequence_window_down(x, CURRENTWINDOW, in[1], 0);
+	if (strcmp(in[2], "0") == 0){
+		if (strcmp(in[1], "mouse1") != 0 &&	strcmp(in[1], "mouse2") != 0 && strcmp(in[1], "mouse3") != 0 && strcmp(in[1], "mouse4") != 0 && strcmp(in[1], "mouse5") != 0){
+			xdo_send_keysequence_window_down(x, CURRENTWINDOW, in[1], 0);
+		}else{
+			xdo_mouse_down(x, CURRENTWINDOW, atoi(&in[1][5]));
+		}
 	}else{
-		xdo_send_keysequence_window_up(x, CURRENTWINDOW, in[1], 0);
+		if (strcmp(in[1], "mouse1") != 0 &&	strcmp(in[1], "mouse2") != 0 && strcmp(in[1], "mouse3") != 0 && strcmp(in[1], "mouse4") != 0 && strcmp(in[1], "mouse5") != 0){
+			xdo_send_keysequence_window_up(x, CURRENTWINDOW, in[1], 0);
+		}else{
+			xdo_mouse_up(x, CURRENTWINDOW, atoi(&in[1][5]));
+		}
 	}
 	return 0;
 }
-

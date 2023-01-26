787a8abadba2c64491c21ed4dfd2a39fc5b2f95e
mckset <99543950+mckset@users.noreply.github.com>
2023-01-25 19:47:35 -0500

    New method
    Switched from sending a key event to a key down event to enable the driver to combine the down key with keys/buttons from other devices

--- output.c
+++ output.c
@@ -1,10 +1,14 @@
 #include <xdo.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int args, char *in[]){
 	xdo_t *x = xdo_new(NULL);
-	xdo_send_keysequence_window(x, CURRENTWINDOW, in[1], 0);
+	if (strcmp(in[2], "0") == 0){		
+		xdo_send_keysequence_window_down(x, CURRENTWINDOW, in[1], 0);
+	}else{
+		xdo_send_keysequence_window_up(x, CURRENTWINDOW, in[1], 0);
+	}
 	return 0;
 }
-
 
